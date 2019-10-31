553601a890c8ac60dfd23d680050884606d7c66e
aravindk73 <45432040+aravindk73@users.noreply.github.com>
2019-10-31 12:15:16 +0530

    Add files via upload

---
+++ cq-array.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 5
+
+int Q[MAX],rear=-1,front=-1 ;
+
+void enqueue();
+void dequeue();
+void display();
+
+void main()
+{
+ int ch ;
+
+  while(1)
+      {
+          printf("\n\n1.Enqueue\n2.Dequeue\n3.Display\n4.Exit");
+		printf("\n\nEnter your choice:");
+		scanf("%d",&ch);
+		
+		switch(ch)
+		{
+			case 1: enqueue();
+					break;
+			case 2: dequeue();
+					break;
+			case 3: display();
+					break;
+			case 4: exit(0);
+			
+			default: printf("\nWrong Choice!!");
+		}
+	}
+}
+void enqueue()
+{
+   int val;
+	
+	if(front==(rear+1)%MAX)
+	{
+		printf("\nQueue is full!!");
+	}
+	else
+	{   
+          printf("\nEnter element :");
+		scanf("%d",&val);    
+            if(front==-1)
+               {front=0;
+               rear=0; } 
+            else 
+		rear=(rear+1)%MAX;
+          Q[rear]=val;
+	}
+}
+
+void dequeue()
+{  
+   int item;	
+   if(front==-1)
+      printf("\nQueue is empty\n");
+   else 
+      item=Q[front] ;
+      if(front==rear)
+       { front=-1;
+        rear=-1; }
+      else
+        front=(front+1)%MAX ;
+      
+}      
+
+void display()
+{
+ int i;
+ if(front==-1 && rear==-1)
+  printf("\nQueue is empty.\n") ;
+ else
+  if(front<=rear)
+   { 
+     for(i=front;i<=rear;i++)
+         printf("%d \n",Q[i]);
+   }
+  else
+   if(front>rear)
+    {
+      for(i=front;i<=MAX-1;i++)
+        printf("%d\n",Q[i]);
+      for(i=rear;i>=0;i--)
+         printf("%d\n",Q[i]); 
+    }  
+}
